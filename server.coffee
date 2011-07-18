496e116cee2fbcea964c38262c0c66ab8dfba4c6
George Ornbo <george@shapeshed.com>
2011-07-18 15:31:54 +0100

    ported server.js to coffeescript

---
+++ server.coffee
@@ -0,0 +1,42 @@
+express = require('express')
+io = require('socket.io')
+
+app = module.exports = express.createServer() 
+
+app.configure () -> 
+  app.set('views', __dirname + '/views')
+  app.set('view engine', 'jade')
+  app.use(express.bodyParser())
+  app.use(express.methodOverride())
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }))
+  app.use(app.router)
+  app.use(express.static(__dirname + '/public'))
+
+app.configure 'development', () -> 
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })) 
+
+app.configure 'production', () -> 
+  app.use(express.errorHandler()) 
+
+io = require('socket.io').listen(app)
+count = 0
+
+io.sockets.on 'connection', (socket) ->
+  count++
+  io.sockets.emit 'count', { number: count }
+
+  setInterval(() ->
+    io.sockets.emit 'count', { number: count }
+  , 1200)
+
+  socket.on 'disconnect', () ->
+    count--
+    io.sockets.emit 'count', { number: count }
+
+app.get '/', (req, res) ->
+  res.render 'index', {title: 'node.js express socket.io counter'}
+
+if not module.parent
+  app.listen 10709
+  console.log "Express server listening on port %d", app.address().port
+
--- server.js
+++ server.js
@@ -1,60 +1,54 @@
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-	, io = require('socket.io')
-
-var app = module.exports = express.createServer();
-
-// Configuration
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
-});
-
-var io = require('socket.io').listen(app);
-var count = 0;
-
-io.sockets.on('connection', function (socket) {
-	count++
-	io.sockets.emit('count', { number: count });
-
-	setInterval(function() {
-		io.sockets.emit('count', { number: count });
-	}, 1000);
-
-	socket.on('disconnect', function () {
-		count--
-		io.sockets.emit('count', { number: count });
-	});
-});
-
-// Routes
-
-app.get('/', function(req, res){
-  res.render('index', {
-    title: 'node.js express socket.io counter'
+(function() {
+  var app, count, express, io;
+  express = require('express');
+  io = require('socket.io');
+  app = module.exports = express.createServer();
+  app.configure(function() {
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(require('stylus').middleware({
+      src: __dirname + '/public'
+    }));
+    app.use(app.router);
+    return app.use(express.static(__dirname + '/public'));
   });
-});
-
-// Only listen on $ node app.js
-
-if (!module.parent) {
-  app.listen(10709);
-  console.log("Express server listening on port %d", app.address().port);
-}
+  app.configure('development', function() {
+    return app.use(express.errorHandler({
+      dumpExceptions: true,
+      showStack: true
+    }));
+  });
+  app.configure('production', function() {
+    return app.use(express.errorHandler());
+  });
+  io = require('socket.io').listen(app);
+  count = 0;
+  io.sockets.on('connection', function(socket) {
+    count++;
+    io.sockets.emit('count', {
+      number: count
+    });
+    setInterval(function() {
+      return io.sockets.emit('count', {
+        number: count
+      });
+    }, 1200);
+    return socket.on('disconnect', function() {
+      count--;
+      return io.sockets.emit('count', {
+        number: count
+      });
+    });
+  });
+  app.get('/', function(req, res) {
+    return res.render('index', {
+      title: 'node.js express socket.io counter'
+    });
+  });
+  if (!module.parent) {
+    app.listen(10709);
+    console.log("Express server listening on port %d", app.address().port);
+  }
+}).call(this);
